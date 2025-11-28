bfc069f7e9b1cdbb053813f8c19073192d9aac0d
Nicollas Prado <127448248+nicollasprado@users.noreply.github.com>
2025-11-28 13:33:08 +0000

    feat: login/logout with clerk

--- carrinho/src/app/page.tsx
+++ carrinho/src/app/page.tsx
@@ -1,3 +1,15 @@
-export default function Home() {
-  return <div>Teste</div>;
+import { auth } from "@clerk/nextjs/server";
+
+export default async function Home() {
+  // await auth.protect()
+
+  return (
+    <div>
+      <header className="flex justify-aroud w-full">
+        <h1>Carrinho</h1>
+
+        <button />
+      </header>
+    </div>
+  )
 }
